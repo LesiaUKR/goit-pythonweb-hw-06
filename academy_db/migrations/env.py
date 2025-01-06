065a0d5d211b91a81731cc1d70786081f12417e2
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-01-06 21:12:47 +0000

    feat: add alembic configuration

--- academy_db/migrations/env.py
+++ academy_db/migrations/env.py
@@ -4,10 +4,21 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from academy_db.database_config import url_to_db
+
+from academy_db.models import Base
+
+import sys
+import os
+
+# Додаємо шлях до кореневого каталогу проєкту
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", url_to_db)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
