adfb9324402d418957c76b5db2e51fb65b230086
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-01-06 20:25:06 +0000

    fix: change models with Mapped

---
+++ academy_db/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+# Завантаження змінних з .env
+load_dotenv()
+
+DATABASE_CONFIG = {
+    'DB_NAME': os.getenv('DB_NAME'),
+    'DB_USER': os.getenv('DB_USER'),
+    'DB_PASSWORD': os.getenv('DB_PASSWORD'),
+    'DB_HOST': os.getenv('DB_HOST'),
+    'DB_PORT': os.getenv('DB_PORT'),
+}
--- academy_db/models.py
+++ academy_db/models.py
@@ -1,41 +1,49 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Float
-from sqlalchemy.orm import relationship, declarative_base
+from datetime import date
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 class Student(Base):
     __tablename__ = 'students'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    group_id = Column(Integer, ForeignKey('groups.id'), nullable=False)
-    group = relationship('Group', back_populates='students')
-    # grades = relationship("Grade", back_populates="student")
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    group_id: Mapped[int] = mapped_column(ForeignKey('groups.id'), nullable=False)
+    group: Mapped["Group"] = relationship(back_populates='students')
+    grades: Mapped[list["Grade"]] = relationship(back_populates="student")
 
 class Group(Base):
     __tablename__ = 'groups'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String,  unique=True, nullable=False)
-    students = relationship('Student', back_populates='group')
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    students: Mapped[list["Student"]] = relationship(back_populates="group")
 
 class Teacher(Base):
     __tablename__ = 'teachers'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    subjects: Mapped[list["Subject"]] = relationship(back_populates="teacher")
 
 class Subject(Base):
     __tablename__ = 'subjects'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    teacher_id = Column(Integer, ForeignKey('teachers.id'), nullable=False)
-    teacher = relationship('Teacher', back_populates="subjects")
-    grades = relationship("Grade", back_populates="subject")
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    teacher_id: Mapped[int] = mapped_column(ForeignKey('teachers.id'), nullable=False)
+    teacher: Mapped["Teacher"] = relationship(back_populates="subjects")
+    grades: Mapped[list["Grade"]] = relationship(back_populates="subject")
 
 class Grade(Base):
     __tablename__ = 'grades'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    student_id = Column(Integer, ForeignKey('students.id'), nullable=False)
-    subject_id = Column(Integer, ForeignKey('subjects.id'), nullable=False)
-    grade = Column(Float, nullable=False)
-    date_received = Column(Date, nullable=False)
-    student = relationship('Student', back_populates="grades")
-    subject = relationship('Subject',back_populates="grades")
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    student_id: Mapped[int] = mapped_column(ForeignKey('students.id'), nullable=False)
+    subject_id: Mapped[int] = mapped_column(ForeignKey('subjects.id'), nullable=False)
+    grade: Mapped[float] = mapped_column(nullable=False)
+    date_received: Mapped[date] = mapped_column(nullable=False)
+    student: Mapped["Student"] = relationship(back_populates="grades")
+    subject: Mapped["Subject"] = relationship(back_populates="grades")
